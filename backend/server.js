3838fd2febced9aafb90d7fb30d6b86ffb943e4d
barbareshet <barbareshet@gmail.com>
2020-11-23 16:24:28 +0200

    Routs Error Handlers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 // import products from './data/products.js';
 import productRoutes from './routes/productRouts.js';
+import {notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
 // import { connect } from "mongoose";
 const app = express();
 
@@ -13,15 +15,24 @@ dotenv.config();
 
 connectDB();
 /**
- *
  * Routs
  */
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
+    });
+}
 
 app.use('/api/products', productRoutes )
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow.bold));
