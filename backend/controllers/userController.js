1b54ebd64605a5c7a817f418edc279d7b1513af4
barbareshet <barbareshet@gmail.com>
2020-12-16 14:17:32 +0200

    User Registartion Backend
    Added User registration in User Controller
    Added User registration route in User Routes
    Added Hash password hook before sending the password in UserModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,6 +29,42 @@ const authUser = asyncHandler(async ( req, res ) => {
     }
 })
 
+//  @desc   Register new User
+//  @route  POST /api/users
+//  @access public
+
+const registerUser = asyncHandler(async ( req, res ) => {
+    const { name,  email, password } = req.body
+
+    // if you send res, you will get a header sent Error
+    // res.send({email, password})
+
+
+    const userExists = await User.findOne({email})
+
+    if (userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if (user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 //  @desc   get user profile
 //  @route  GET /api/users/profile
 //  @access private
@@ -51,5 +87,6 @@ const getUserProfile = asyncHandler(async ( req, res ) => {
 
 export {
     authUser,
-    getUserProfile
+    getUserProfile,
+    registerUser
 }
