74bf9414141d273b54a6ad107a4db70cf880ddea
barbareshet <barbareshet@gmail.com>
2021-01-23 22:44:28 +0200

    User login + logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
-import {Navbar, Nav, Container, Badge} from "react-bootstrap";
-import { useSelector } from "react-redux";
+import {Navbar, Nav, Container, Badge, NavDropdown} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from 'react-router-bootstrap';
+import {logOut} from "../actions/userAction";
 const Header = () =>{
+    const dispatch = useDispatch();
     const cart = useSelector( state => state.cart )
     const { cartItems } = cart;
+    const userLogin = useSelector( state => state.userLogin );
+    const { userInfo } = userLogin;
+    function logoutHandler(e){
+        dispatch(logOut());
+    }
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
@@ -21,14 +28,31 @@ const Header = () =>{
                                     {!!cartItems > 0 &&
                                         <Badge variant="info"> {cartItems.reduce((acc, item) => acc + item.qty, 0)}</Badge>
                                     }
-
                                 </Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link>
-                                    <i className="fas fa-user mr-2" aria-hidden="true"></i>Sign In
-                                </Nav.Link>
-                            </LinkContainer>
+                            { userInfo ? (
+                               <NavDropdown id="username" title={userInfo.name}>
+                                   <LinkContainer to="/profile">
+                                       <NavDropdown.Item><i className="fas fa-user mr-2" aria-hidden="true"></i>Profile</NavDropdown.Item>
+                                   </LinkContainer>
+                                   <NavDropdown.Item onClick={logoutHandler}>
+                                       <i className="fas fa-sign-out-alt mr-2" aria-hidden="true"></i>Logout
+                                   </NavDropdown.Item>
+                               </NavDropdown>
+                            ) : (
+                                <>
+                                    <LinkContainer to="/login">
+                                        <Nav.Link>
+                                            <i className="fas fa-sign-in-alt mr-2" aria-hidden="true"></i>Sign In
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                    <LinkContainer to="/register">
+                                        <Nav.Link>
+                                            / Register
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                </>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/screens/login.js
+++ frontend/src/screens/login.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {
     Form,
     Button,
@@ -12,20 +12,30 @@ import Loader from "../components/Loader";
 import { login } from "../actions/userAction";
 import FormContainer from "../components/formContainer";
 
-const LoginScreen = ({location}) => {
+const LoginScreen = ({location, history}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const redirect = location.search ? location.search.split('=')[1] : '/';
 
+    const dispatch = useDispatch();
+    const userLogin = useSelector( state => state.userLogin );
+    const { loading, error, userInfo } = userLogin;
 
-
+    useEffect( ()=>{
+        if (userInfo){
+            history.push(redirect);
+        }
+    }, [history, userInfo, redirect ])
     const submitHandler = (e)=> {
         e.preventDefault();
-        console.log(e.target.value);
+        // console.log(e.target.value);
+        dispatch(login(email, password));
     }
     return (
         <FormContainer>
             <h1>Login</h1>
+            {error && <Message varient="danger">{error}</Message>}
+            {loading && <Loader/>}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="email">
                     <Form.Label>Email Address</Form.Label>
